3c289b0dbcdeacd0fc067874dfe1145bf1dbac3d
Davin <davinkusno@gmail.com>
2024-04-30 12:05:22 +0700

    Environment variable update

--- src/models/user.js
+++ src/models/user.js
@@ -95,7 +95,7 @@ userSchema.methods.toJSON = function () {
 
 userSchema.methods.generateAuthToken = async function () {
 	const user = this
-	const token = jwt.sign({ _id: user._id.toString() }, 'taskmanagerapi')
+	const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET)
 
 	user.tokens = user.tokens.concat({ token })
 	await user.save()
